356c75401b86c01d82acc271470936ef8ef2dda6
liuhongchaung <512378379@qq.com>
2024-10-08 21:23:25 +0800

    登录

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,28 +1,44 @@
 <template>
     <div class="login-container">
-        <el-form :model="ruleForm" :rules="rules"
+        <el-form :model="loginForm" :rules="rules"
                  status-icon
-                 ref="ruleForm"
+                 ref="loginForm"
                  label-position="left"
                  label-width="0px"
                  class="login-page">
             <h3 class="text-center">系统登录</h3>
-            <el-form-item prop="username">
+
+            <el-form-item prop="loginName">
                 <el-input type="text"
-                          v-model="ruleForm.username"
+                          v-model="loginForm.loginName"
                           placeholder="用户名"
                 ></el-input>
             </el-form-item>
+
             <el-form-item prop="password">
                 <el-input type="password"
-                          v-model="ruleForm.password"
+                          v-model="loginForm.password"
                           placeholder="密码">
                 </el-input>
             </el-form-item>
 
+            <el-form-item prop="checkCode">
+                <el-input type="checkCode" style="width:45%;" @input="handleInput"
+                          v-model="loginForm.checkCode"
+                          placeholder="验证码">
+                </el-input>
+            </el-form-item>
 
-            <el-form-item style="width:100%;">
-                <el-button type="primary" style="width:100%;" @click="hi()">登录</el-button>
+            <el-form-item prop="role">
+                <el-radio-group v-model="loginForm.role" class="radio-margin">
+                    <el-radio value="2">普通用户</el-radio>
+                    <el-radio value="1">管理员</el-radio>
+                </el-radio-group>
+            </el-form-item>
+
+            <el-form-item style="width:100%;" class="button-margin">
+                <el-button type="primary" style="width:30%;" @click="login">登录</el-button>
+                <el-button type="primary" style="width:30%;" >注册</el-button>
             </el-form-item>
 
 
@@ -36,21 +52,31 @@
         name: "login",
         data(){
             return{
-                ruleForm: {
-                    username: '',
-                    password: ''
+                loginForm: {
+                    loginName: '',
+                    password: '',
+                    role:'2',
+                    checkCode:'',
                 },
                 rules: {
-                    username: [{required: true, message: '请输入用户名', trigger: 'blur'}],
-                    password: [{required: true, message: '请输入密码', trigger: 'blur'}]
+                    loginName: [{required: true, message: '请输入用户名', trigger: 'blur'}],
+                    password: [{required: true, message: '请输入密码', trigger: 'blur'}],
+                    checkCode: [{required: true, message: '请输入验证码', trigger: 'blur'}],
                 }
             }
         },
         methods: {
-            hi(){
-                alert(this.ruleForm.username )
-                alert(this.ruleForm.password )
+            login(){
+                alert(this.loginForm.loginName )
+                alert(this.loginForm.password )
+            },
+            handleInput() {
+                //直接在input事件中处理，防止用户输入汉字
+                this.loginForm.checkCode = this.loginForm.checkCode.replace(/[\u4e00-\u9fa5]/g, '');
+                //不允许输入特殊符号
+                this.loginForm.checkCode = this.loginForm.checkCode.replace(/[^\w\u4E00-\u9FA5]/g, '')
             }
+
         }
     }
 </script>
@@ -67,10 +93,17 @@
         -webkit-border-radius: 5px;
         border-radius: 5px;
         margin: 180px auto;
-        width: 350px;
+        width: 250px;
+        height: 300px;
         padding: 35px 35px 15px;
         background: #fff;
         border: 1px solid #eaeaea;
         box-shadow: 0 0 25px #cac6c6;
+    }
+    .radio-margin {
+        margin-left: 10%;
+    }
+    .button-margin {
+        margin-left: 16%;
     }
 </style>
