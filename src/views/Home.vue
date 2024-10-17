e666e125440b5069bc2e046d734018b34cd1226e
liuhongchuang <liuhongchuang>
2024-10-17 18:27:42 +0800

    菜单修改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,31 @@
       <!--左侧-->
       <el-container>
         <el-aside width="200px">
-          <el-menu router="true">
+          <el-menu router>
+            <div v-for="item in this.routerItems">
+              <el-sub-menu v-if="item.children" :index="item.path">
+                <template #title>
+                  <el-icon><location/></el-icon>
+                  <span>{{item.name}}</span>
+                </template>
+
+                <el-menu-item v-for="child in item.children" :index="child.path">
+                  {{child.name}}
+                </el-menu-item>
+
+              </el-sub-menu>
+
+              <el-menu-item v-else :index="item.path">
+                {{item.name}}
+              </el-menu-item>
+
+            </div>
+
+
+
+          </el-menu>
+
+          <!--<el-menu router>
 
             <el-sub-menu>
               <template #title>
@@ -19,20 +43,20 @@
               <el-menu-item index="/test1"><el-icon><Setting/></el-icon>第一级-1</el-menu-item>
               <el-menu-item index="/test2"><el-icon><Setting/></el-icon>第一级-2</el-menu-item>
 
-              <el-sub-menu index="1-3">
+              &lt;!&ndash;<el-sub-menu index="1-3">
                 <template #title>
                   <el-icon><location/></el-icon>
                   <span>第一级-3</span>
                 </template>
                 <el-menu-item index="1-3-1">第一级-3-1</el-menu-item>
                 <el-menu-item index="1-3-2">第一级-3-2</el-menu-item>
-              </el-sub-menu>
+              </el-sub-menu>&ndash;&gt;
 
             </el-sub-menu>
 
             <el-menu-item index="/test3"><el-icon><Setting/></el-icon>用户管理</el-menu-item>
 
-          </el-menu>
+          </el-menu>-->
         </el-aside>
 
         <!--右侧-->
@@ -47,19 +71,24 @@
 
 <script>
   import {Location, Setting} from '@element-plus/icons-vue';
+  import {useRouter} from "vue-router";
 export default {
   name: 'Home',
   components: {
     //图标
     Location, Setting
   },
-  methods:{
-    pushrouter(){
-      this.$router.push('/test1');
-    },
-    pushrouter2(){
-      this.$router.push('/test2');
+  mounted() {
+    this.routerItems = useRouter().options.routes;
+    console.log(this.routerItems)
+  },
+  data(){
+    return{
+      routerItems:[],
     }
+  },
+  methods:{
+
   }
 }
 </script>
